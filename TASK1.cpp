60207e00ac2635c6da172fb8545d816fb2327a02
Teodorddp <teodorgjorcheski@gmail.com>
2026-05-08 12:58:45 +0200

    Update TASK1.cpp

--- TASK1.cpp
+++ TASK1.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main() {
 cout << "Hello GitHub!" << endl;
   cout << "This is my second commit." << endl;
-  cout << "I am learning C++ and GitHub." << endl;
+  
 cout << "This is my first C++ file." << endl;
 return 0;
 }
